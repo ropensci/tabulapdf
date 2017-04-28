80f369ac350bd20e31aff0a74381130cc4d9299f
jkeuskamp <joost@keuskamp.com>
2017-04-29 01:24:35 +0200

    Add option to increase predictability of thumbnail filenames
    Thumbnails are named by their file of origin with page number as
    suffix. By default files are renamed such that the page suffix contains
    the same number of characters for all thumbnails (01,02,…,10,11). As a
    result the first thumbnail could either be called <somename>1.ext or
    <somename>01.ext or even <somename>001.ext, making it harder to pick up
    these files downstream. Here an option is added to change this
    behaviour so that the first thumbnail is always called <somename>1.ext

--- R/make_thumbnails.R
+++ R/make_thumbnails.R
@@ -6,6 +6,7 @@
 #' @param format A character string specifying an image file format.
 #' @param resolution An integer specifying the image resolution in DPI.
 #' @param password Optionally, a character string containing a user password to access a secured PDF.
+#' @param uniform determines whether the page numbers suffixes of generated thumbnails have a uniform (01,02,...,10) or short (1,2,...,10) format. 
 #' @details This function save each (specified) page of a document as an image with 720 dpi resolution. Images are saved in the same directory as the original file, with file names specified by the original file name, a page number, and the corresponding file format extension.
 #' @note This may generate Java \dQuote{INFO} messages in the console, which can be safely ignored.
 #' @return A character vector of file paths.
@@ -23,7 +24,7 @@
 #' @seealso \code{\link{extract_tables}}, \code{\link{extract_text}}, \code{\link{make_thumbnails}}
 #' @export
 make_thumbnails <- 
-function(file, outdir = NULL, pages = NULL, format = c("png", "jpeg", "bmp", "gif"), resolution = 72L, password = NULL) {
+function(file, outdir = NULL, pages = NULL, format = c("png", "jpeg", "bmp", "gif"), resolution = 72L, password = NULL, uniform = TRUE) {
     file <- localize_file(file)
     pdfDocument <- load_doc(file, password = password)
     on.exit(pdfDocument$close())
@@ -31,7 +32,7 @@ function(file, outdir = NULL, pages = NULL, format = c("png", "jpeg", "bmp", "gi
     if (!is.null(pages)) {
         pages <- as.integer(pages)
     } else {
-        pages <- 1L:(get_n_pages(doc = pdfDocument))
+        pages <- 1L:((doc = pdfDocument))
     }
     
     format <- match.arg(format)
@@ -48,6 +49,6 @@ function(file, outdir = NULL, pages = NULL, format = c("png", "jpeg", "bmp", "gi
         PDFImageWriter$writeImage(pdfDocument, format, "", pages[i], pages[i], 
                                   prefix, 1L, as.integer(resolution))
     }
-    file.rename(from = paste0(prefix, pages, ".", format), to = outfile)
-    ifelse(file.exists(outfile), outfile, NA_character_)
+    if(isTRUE(uniform)){file.rename(from = paste0(prefix, pages, ".", format), to = outfile)
+    ifelse(file.exists(outfile), outfile, NA_character_)}
 }
