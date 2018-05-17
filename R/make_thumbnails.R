7e602e9ab0bdd5e5ee18a0a7cb2525f10e082876
Tom Paskhalis <tpaskhalis@gmail.com>
2018-05-17 12:16:06 +0100

    Change outdir to tempdir() instead of file origin

--- R/make_thumbnails.R
+++ R/make_thumbnails.R
@@ -1,13 +1,21 @@
 #' @title make_thumbnails
 #' @description Convert Pages to Image Thumbnails
 #' @param file A character string specifying the path or URL to a PDF file.
-#' @param outdir An optional character string specifying a directory into which to split the resulting files. If \code{NULL}, the directory of the original PDF is used, unless \code{file} is a URL in which case a temporary directory is used.
+#' @param outdir An optional character string specifying a directory into which
+#' to split the resulting files. If \code{NULL}, the \code{outdir} is
+#' \code{tempdir()}. If \code{file} is a URL, both file and thumbnails are
+#' stored in the R session's temporary directory.
 #' @param pages An optional integer vector specifying pages to extract from.
 #' @param format A character string specifying an image file format.
 #' @param resolution A numeric value specifying the image resolution in DPI.
-#' @param password Optionally, a character string containing a user password to access a secured PDF.
-#' @details This function save each (specified) page of a document as an image with 720 dpi resolution. Images are saved in the same directory as the original file, with file names specified by the original file name, a page number, and the corresponding file format extension.
-#' @note This may generate Java \dQuote{INFO} messages in the console, which can be safely ignored.
+#' @param password Optionally, a character string containing a user password
+#' to access a secured PDF.
+#' @details This function save each (specified) page of a document as an image
+#' with 720 dpi resolution. Images are saved in the same directory as the
+#' original file, with file names specified by the original file name,
+#' a page number, and the corresponding file format extension.
+#' @note This may generate Java \dQuote{INFO} messages in the console,
+#' which can be safely ignored.
 #' @return A character vector of file paths.
 #' @references \href{http://tabula.technology/}{Tabula}
 #' @author Thomas J. Leeper <thosjleeper@gmail.com>
@@ -20,7 +28,8 @@
 #' }
 #' @importFrom tools file_path_sans_ext
 #' @importFrom rJava J new .jfloat
-#' @seealso \code{\link{extract_tables}}, \code{\link{extract_text}}, \code{\link{make_thumbnails}}
+#' @seealso \code{\link{extract_tables}}, \code{\link{extract_text}},
+#' \code{\link{make_thumbnails}}
 #' @export
 make_thumbnails <- function(file,
                             outdir = NULL,
@@ -41,11 +50,11 @@ make_thumbnails <- function(file,
     format <- match.arg(format)
     fileseq <- formatC(pages, width = max(nchar(pages)), flag = 0)
     if (is.null(outdir)) {
-        outfile <- paste0(file_path_sans_ext(file), fileseq, ".", format)
-    } else {
-        filename <- paste0(basename(file_path_sans_ext(file)), fileseq, ".", format)
-        outfile <- file.path(outdir, filename)
+        outdir <- tempdir()
     }
+    filename <- paste0(file_path_sans_ext(basename(file)), fileseq, ".", format)
+    outfile <- file.path(outdir, filename)
+
     for (i in seq_along(pages)) {
         pageIndex <- pages[i] - 1L
         PDFRenderer <- new(J("org.apache.pdfbox.rendering.PDFRenderer"),
